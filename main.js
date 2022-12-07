2e412d06355486ccbd9c35489e2ab71fb9f90934
Panchcami <panchcami1@gmail.com>
2022-12-07 00:14:33 -0600

    We added clase8.html to the index.html to add the mobile menu, we modified main.js and styles.css to make it functional

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ menuEmail.addEventListener('click', toggleDesktopMenu);
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
 }
+
+const menuImage = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu')
+
+menuImage.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
+}
